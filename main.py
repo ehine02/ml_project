e3aa58b562fdaeaab48e90e035ef22997b6ec445
ehine02 <56832762+ehine02@users.noreply.github.com>
2021-02-16 19:32:09 +0000

    Running version

--- main.py
+++ main.py
@@ -2,12 +2,10 @@ import tensorflow as tf
 from tensorflow import feature_column
 from tensorflow import keras
 from tensorflow.keras import layers
-from tensorflow.python.keras.optimizer_v2.optimizer_v2 import OptimizerV2
-
 import pandas as pd
 import numpy as np
-
-
+from opt import WAMEFromRMSProp, WAMEOptimizer
+from tensorflow.python.keras.optimizer_v2.rmsprop import RMSProp
 #
 # Adapted from
 #       https://www.tensorflow.org/tutorials/structured_data/feature_columns
@@ -64,31 +62,24 @@ if __name__ == '__main__':
 
     feature_layer = tf.keras.layers.DenseFeatures(feature_columns)
 
-    batch_size = 1000
+    batch_size = 32
     train_ds = df_to_dataset(df, batch_size=batch_size)
     val_ds = df_to_dataset(df, shuffle=False, batch_size=batch_size)
 
     model = tf.keras.Sequential([
         feature_layer,
-        layers.Dense(7 ** 4, activation=keras.activations.relu),
-        layers.Dense(7 ** 3, activation=keras.activations.relu),
+        layers.Dense(14, activation=keras.activations.relu),
+        layers.Dense(14, activation=keras.activations.relu),
         layers.Dropout(.1),
-        layers.Dense(1)
+        layers.Dense(1, activation=keras.activations.sigmoid)
     ])
 
-    lr_schedule = keras.optimizers.schedules.ExponentialDecay(
-        initial_learning_rate=1e-2,
-        decay_steps=10000,
-        decay_rate=0.9)
-
-    model.compile(optimizer=keras.optimizers.Adam(),
+    model.compile(optimizer=WAMEOptimizer(),
                   loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
                   metrics=['accuracy'])
 
     # train/validate
-    model.fit(train_ds,
-              validation_data=val_ds,
-              epochs=10)
+    model.fit(train_ds, validation_data=val_ds, epochs=10)
 
     # test
     test_df = get_clean_data(name='adult.test', skiprows=1, salary_str='<=50K.')
@@ -97,40 +88,4 @@ if __name__ == '__main__':
     print("Accuracy", accuracy)
 
 
-class CustomModel(keras.Model):
-    def train_step(self, data):
-        # Unpack the data. Its structure depends on your model and
-        # on what you pass to `fit()`.
-        x, y = data
-
-        with tf.GradientTape() as tape:
-            y_pred = self(x, training=True)  # Forward pass
-            # Compute the loss value
-            # (the loss function is configured in `compile()`)
-            loss = self.compiled_loss(y, y_pred, regularization_losses=self.losses)
-
-        # Compute gradients
-        trainable_vars = self.trainable_variables
-        gradients = tape.gradient(loss, trainable_vars)
-        # Update weights
-        self.optimizer.apply_gradients(zip(gradients, trainable_vars))
-        # Update metrics (includes the metric that tracks the loss)
-        self.compiled_metrics.update_state(y, y_pred)
-        # Return a dict mapping metric names to current value
-        return {m.name: m.result() for m in self.metrics}
-
-
-class WAMEOptimizer(OptimizerV2):
-    def __init__(self, learning_rate=0.01, name='WAMEOptimizer', **kwargs):
-        super().__init__(name, **kwargs)
-        self.set_hyper('learning_rate', learning_rate)
-
-    def _create_slots(self, var_list):
-        pass
-
-    def _resource_apply_dense(self, grad, handle, apply_state):
-        pass
-
-    def _resource_apply_sparse(self, grad, handle, indices, apply_state):
-        pass
 
