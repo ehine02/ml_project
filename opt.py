fb1ded3aba446a129620f64cb38292e8eb34a9ea
Ed <ehine02@dcs.bbk.ac.uk>
2021-02-16 09:53:03 +0000

    Tweaked case statement

--- opt.py
+++ opt.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 from tensorflow.python.keras.optimizer_v2 import optimizer_v2
+import keras.backend as K
+from tensorflow.python.ops import state_ops
 
 #
 # Adapted from
@@ -20,11 +22,11 @@ class WAMEOptimizer(optimizer_v2.OptimizerV2):
     def __init__(self, learning_rate=0.01, name='WAMEOptimizer', **kwargs):
         super().__init__(name, **kwargs)
         self._set_hyper('learning_rate', learning_rate)
-        self._set_hyper('eta_plus', 1.2)
-        self._set_hyper('eta_minus', 0.1)
-        self._set_hyper('eta_max', 100.0)
-        self._set_hyper('eta_min', 0.01)
-        self._set_hyper('alpha', 0.9)
+        self._set_hyper('eta_plus', tf.Variable(1.2))
+        self._set_hyper('eta_minus', tf.Variable(0.1))
+        self._set_hyper('eta_max', tf.Variable(100.0))
+        self._set_hyper('eta_min', tf.Variable(0.01))
+        self._set_hyper('alpha', tf.Variable(0.9))
 
     def _create_slots(self, var_list):
         [self.add_slot(var, 'zeta', initializer=tf.ones) for var in var_list]
@@ -32,30 +34,31 @@ class WAMEOptimizer(optimizer_v2.OptimizerV2):
         [self.add_slot(var, 'z', initializer=tf.zeros) for var in var_list]
         [self.add_slot(var, 'prev_grad', initializer=tf.ones) for var in var_list]
 
-    @tf.function
     def _resource_apply_dense(self, grad, var, apply_state):
         prev_grad = self.get_slot(var, 'prev_grad')
         prev_zeta = self.get_slot(var, 'zeta')
         prev_z = self.get_slot(var, 'z')
         prev_beta = self.get_slot(var, 'beta')
 
-        def get_zeta():
-            if tf.greater(grad * prev_grad, 0):
-                return tf.minimum(prev_zeta * self._get_hyper('eta_plus'), self._get_hyper('eta_max'))
-            elif tf.less(grad * prev_grad, 0):
-                return tf.maximum(prev_zeta * self._get_hyper('eta_minus'), self._get_hyper('eta_min'))
-            else:
-                return tf.minimum(prev_zeta * self._get_hyper('eta_plus'), self._get_hyper('eta_max'))
+        def grad_min():
+            return tf.minimum(prev_zeta * self._get_hyper('eta_plus'), self._get_hyper('eta_max'))
 
-        zeta_t = get_zeta()
+        def grad_max():
+            return tf.maximum(prev_zeta * self._get_hyper('eta_minus'), self._get_hyper('eta_min'))
+
+        zeta_t = tf.case([(tf.greater(grad * prev_grad, 0), grad_min),
+                          (tf.less(grad * prev_grad, 0), grad_max)],
+                         exclusive=False)
         z_t = self._get_hyper('alpha') * prev_z + (1 - self._get_hyper('alpha')) * zeta_t
-        beta_t = self._get_hyper('alpha') * prev_beta + (1 - self._get_hyper('alpha')) * grad**2
+        beta_t = self._get_hyper('alpha') * prev_beta + (1 - self._get_hyper('alpha')) * grad ** 2
         dw_t = (-1 * self._get_hyper('learning_rate') / z_t) * grad * (1 / beta_t)
-        var.assign(var + dw_t)
+        var_t = var + dw_t
+        var.assign(var_t)
         prev_grad.assign(grad)
         prev_zeta.assign(zeta_t)
         prev_z.assign(z_t)
         prev_beta.assign(beta_t)
+        return state_ops.assign(var, var_t, use_locking=self._use_locking).op
 
     def _resource_apply_sparse(self, grad, handle, indices, apply_state):
         raise NotImplementedError
