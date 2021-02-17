3cb860cd25f75715e7796647e4f3d3d32628cf13
ehine02 <56832762+ehine02@users.noreply.github.com>
2021-02-17 20:26:50 +0000

    Tidying

--- opt.py
+++ opt.py
@@ -1,16 +1,17 @@
 import tensorflow as tf
 from tensorflow.python.framework import ops
 from tensorflow.python.keras.optimizer_v2 import optimizer_v2
-import keras.backend as K
-from tensorflow.python.keras.optimizer_v2.rmsprop import RMSProp
-from tensorflow.python.ops import state_ops, control_flow_ops, math_ops, array_ops
+from tensorflow.python.ops import state_ops, math_ops, array_ops
 
 
 #
-# Adapted from
+# Adapted with reference to Tensorflow v2 tutorials:
 #       https://www.tensorflow.org/tutorials/structured_data/feature_columns
 #       https://www.tensorflow.org/guide/keras/customizing_what_happens_in_fit
 #       https://keras.io/api/optimizers/
+#       https://www.tensorflow.org/api_docs/python/tf/keras/optimizers/RMSprop
+# Combined with additional inspiration from:
+#       https://github.com/nitbix/keras-oldfork/blob/master/keras/optimizers.py
 #
 
 
@@ -18,55 +19,66 @@ class WAME(optimizer_v2.OptimizerV2):
     def __init__(self, learning_rate=0.01, name='WAME', **kwargs):
         super().__init__(name, **kwargs)
         self._set_hyper('learning_rate', learning_rate)
-        self._set_hyper('eta_plus', tf.Variable(1.2))
-        self._set_hyper('eta_minus', tf.Variable(0.1))
+        self._set_hyper('eta_pos', tf.Variable(1.2))
+        self._set_hyper('eta_neg', tf.Variable(0.1))
         self._set_hyper('eta_max', tf.Variable(100.0))
         self._set_hyper('eta_min', tf.Variable(0.01))
-        self._set_hyper('rho', tf.Variable(0.9))
+        self._set_hyper('alpha', tf.Variable(0.9))
         self.epsilon = 1e-7
 
     def get_config(self):
         base_config = super().get_config()
         return {
             **base_config,
-            "learning_rate": self._serialize_hyperparameter("learning_rate"),
+            'learning_rate': self._serialize_hyperparameter('learning_rate'),
+            'eta_pos': self._serialize_hyperparameter('eta_pos'),
+            'eta_neg': self._serialize_hyperparameter('eta_neg'),
+            'eta_max': self._serialize_hyperparameter('eta_max'),
+            'eta_min': self._serialize_hyperparameter('eta_min'),
+            'alpha': self._serialize_hyperparameter('alpha'),
         }
 
     def _create_slots(self, var_list):
-        [self.add_slot(var, 'prev_grad', initializer=tf.ones) for var in var_list]
+        [self.add_slot(var, 'grad', initializer=tf.ones) for var in var_list]
         [self.add_slot(var, 'beta', initializer=tf.zeros) for var in var_list]
         [self.add_slot(var, 'zeta', initializer=tf.ones) for var in var_list]
-        [self.add_slot(var, 'zed', initializer=tf.zeros) for var in var_list]
+        [self.add_slot(var, 'zedd', initializer=tf.zeros) for var in var_list]
 
     def _prepare_local(self, var_device, var_dtype, apply_state):
         super(WAME, self)._prepare_local(var_device, var_dtype, apply_state)
-        rho = array_ops.identity(self._get_hyper('rho', var_dtype))
         apply_state[(var_device, var_dtype)].update(
             dict(
                 epsilon=ops.convert_to_tensor_v2(self.epsilon, var_dtype),
-                rho=rho,
-                one_minus_rho=1. - rho))
+                eta_pos=self._get_hyper('eta_pos', var_dtype),
+                eta_neg=self._get_hyper('eta_neg', var_dtype),
+                eta_min=self._get_hyper('eta_min', var_dtype),
+                eta_max=self._get_hyper('eta_max', var_dtype),
+                alpha=self._get_hyper('alpha', var_dtype),
+                one_minus_alpha=1. - self._get_hyper('alpha', var_dtype)))
 
-    def _resource_apply_dense(self, grad, var, apply_state):
-        var_device, var_dtype = var.device, var.dtype.base_dtype
-        coefficients = ((apply_state or {}).get((var_device, var_dtype))
-                        or self._fallback_apply_state(var_device, var_dtype))
-        prev_grad = self.get_slot(var, 'prev_grad')
-        beta = self.get_slot(var, 'beta')
-        zeta = self.get_slot(var, 'zeta')
-        zedd = self.get_slot(var, 'zed')
-        gt_g = grad * prev_grad
+    def _resource_apply_dense(self, grad_t, vars, apply_state):
+        hyper = (apply_state.get((vars.device, vars.dtype.base_dtype)))
+
+        grad = self.get_slot(vars, 'grad')
+        beta = self.get_slot(vars, 'beta')
+        zeta = self.get_slot(vars, 'zeta')
+        zedd = self.get_slot(vars, 'zedd')
+
+        gt_g = grad_t * grad
         zeta_t = tf.where(tf.greater(gt_g, tf.zeros_like(gt_g)),
-                          tf.minimum(zeta * self._get_hyper('eta_plus'), self._get_hyper('eta_max')),
-                          tf.maximum(zeta * self._get_hyper('eta_minus'), self._get_hyper('eta_min')))
-        zedd_t = coefficients['rho'] * zedd + coefficients['one_minus_rho'] * zeta_t
-        beta_t = coefficients['rho'] * beta + coefficients['one_minus_rho'] * grad**2
-        var_t = var - (coefficients['lr_t'] / zedd_t) * grad / (math_ops.sqrt(beta_t) + coefficients['epsilon'])
+                          tf.minimum(zeta * hyper['eta_pos'], hyper['eta_max']),
+                          tf.maximum(zeta * hyper['eta_neg'], hyper['eta_min']))
+
+        zedd_t = hyper['alpha'] * zedd + hyper['one_minus_alpha'] * zeta_t
+        beta_t = hyper['alpha'] * beta + hyper['one_minus_alpha'] * grad_t ** 2
+        vars_t = vars - (hyper['lr_t'] / zedd_t) * grad_t / (math_ops.sqrt(beta_t) + hyper['epsilon'])
+
+        grad.assign(grad_t, use_locking=self._use_locking)
         beta.assign(beta_t, use_locking=self._use_locking)
-        prev_grad.assign(grad, use_locking=self._use_locking)
         zeta.assign(zeta_t, use_locking=self._use_locking)
         zedd.assign(zedd_t, use_locking=self._use_locking)
-        return state_ops.assign(var, var_t, use_locking=self._use_locking).op
+
+        return state_ops.assign(vars, vars_t, use_locking=self._use_locking).op
 
     def _resource_apply_sparse(self, grad, handle, indices, apply_state):
         raise NotImplementedError
